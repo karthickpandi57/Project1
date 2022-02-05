c7350aecfc5885f2d85b7e132c8a543e47bc8dca
karthickpandi57 <karthickpandi5898@gmail.com>
2022-02-05 19:04:10 +0530

    message

---
+++ src/org/git/GitClone.java
@@ -0,0 +1,19 @@
+package org.git;
+
+public class GitClone {
+
+	public void employee() {
+		System.out.println(50);
+	}
+	
+	public void clients() {
+		System.out.println(5);
+	}
+	public static void main(String[]args) {
+		GitClone g = new GitClone();
+		g.employee();
+		g.clients();
+	}
+	
+
+}
